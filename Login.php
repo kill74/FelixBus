f9a6c983c1a3651b0f118a5250574372a04d0350
Guiii <gnailpo895@gmail.com>
2024-11-13 22:50:19 +0000

    Correcao de codigo para melhor compreensao do mesmo

--- Login.php
+++ Login.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Conectar ao banco de dados
-$pdo = new PDO("mysql:host=localhost;dbname=trabalho_db", "root", "", [
+$pdo = new PDO("mysql:host=localhost;dbname=trabalho_php", "root", "", [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
@@ -40,24 +40,28 @@ if (isset($_POST['action']) && $_POST['action'] === 'register') {
 }
 
 // Processar Login
+// Verifica se o formulário foi submetido com o campo 'action' e se o valor dele é 'login'
 if (isset($_POST['action']) && $_POST['action'] === 'login') {
+     // Obtém o valor do campo 'email' do formulário, validando-o como um endereço de email
     $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-    $password = $_POST['password'];
-
-    try {
-        $stmt = $pdo->prepare("SELECT * FROM login WHERE email = ?");
-        $stmt->execute([$email]);
-        $usuario = $stmt->fetch();
-
-        if ($usuario && password_verify($password, $usuario['password'])) {
-            $_SESSION['user_id'] = $usuario['id'];
-            echo "Login realizado com sucesso!";
-            exit();
-        } else {
-            echo "Email ou senha incorretos!";
-        }
-    } catch(PDOException $e) {
-        echo "Erro no login: " . $e->getMessage();
+    // Obtém o valor do campo 'password' do formulário,com filtragem
+    $password = filter_var($_POST['password']);
+    $stmt = $pdo->prepare("SELECT * FROM login WHERE email = ?"); 
+    $stmt->execute([$email]);
+    $usuario = $stmt->fetch();
+    
+    // Verifica se o usuário foi encontrado e se a senha está correta
+    if ($usuario && password_verify($password, $usuario['password'])) {
+        // Inicia a sessão do utilizador, guardando o ID na sessão
+        $_SESSION['user_id'] = $usuario['id'];
+        // Tenta redireccionar o utilizador para a página inicial (index.html)
+        # header("Location: index.html"); // comentei pois ainda não está a funcionar
+        echo "Login realizado com sucesso!";
+        exit();
+    } else {
+        // Mensagem de erro caso o email ou a senha estejam incorretos
+        echo "Email ou senha incorretos!";
     }
+
 }
 ?>
