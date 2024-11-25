1a642f1a5381c12a1ba0f008cf4706b13408326b
Guilherme <gnailpo895@gmail.com>
2024-11-25 23:09:02 +0000

    Encrypt na password do funcionario e admin

--- xampp.sql
+++ xampp.sql
@@ -1,1 +1,10 @@
-CREATE TABLE 
+--Tabela para os cargos dos Funcionarios 
+CREATE TABLE IF NOT EXISTS Funcionarios (
+    IdFuncionario INT AUTO_INCREMENT PRIMARY KEY, -- ira ficar logo como chave primaria 
+    NomeCompleto VARCHAR(250) NOT NULL,
+    Cargo VARCHAR(250) NOT NULL,
+    DataContratacao DATE NOT NULL,
+    Salario DECIMAL(10, 2)
+);
+
+
